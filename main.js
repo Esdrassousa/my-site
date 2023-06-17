109248e1801792fb1cd679139eb4d651155cd9ce
Esdras <esdrassousasoares@hotmail.com>
2023-06-17 16:53:39 -0300

    esterelizando site

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-function clickTeclado(letra) {
-    alert(letra);
+const btnMobile = document.getElementById('btn-mobile');
+
+
+//btnMobile.addEventListener('click', togleMenu);
+
+window.addEventListener("click", (event) => {
+  const btnMobile = document.getElementById('btn-mobile');
+  if (btnMobile) {
+    btnMobile.addEventListener('click', togleMenu);
   }
+});
+
+function togleMenu(){
+  console.log("oi")
+  const nav = document.getElementById('navegacao');
+  nav.classList.toggle('active')
+
+}
+
