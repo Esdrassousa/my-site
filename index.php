9d51a1424e277b6cf961ea175250da38eae0139d
esdrassousasoares <you@example.comesdrassousasoares@hotmail.com>
2024-01-14 19:31:54 -0300

    novo bloco 2

--- index.php
+++ index.php
@@ -27,36 +27,64 @@
 
     <div class="heading">
 
-    <div class="inicio-objetivo">
-        <div class="inicio">
+        <div class="inicio-objetivo">
+            <div class="inicio">
 
-            <h2>ELABORAÇÃO DE PROJETOS:</h2>
-            <h2>FOTOVOLTAICOS + DE 650 PROJETOS APROVADOS.</h2>
-            <h2>SUBESTAÇÃO.</h2>
-            <h2>QUADRO AGRUPADO (PMUC).</h2>
-            <h2> SPDA.</h2>
+                <h2>ELABORAÇÃO DE PROJETOS:</h2>
+                <h2>FOTOVOLTAICOS + DE 650 PROJETOS APROVADOS.</h2>
+                <h2>SUBESTAÇÃO.</h2>
+                <h2>QUADRO AGRUPADO (PMUC).</h2>
+                <h2> SPDA.</h2>
+
+            </div>
+
+
+
+            <div class="objetivo">
+
+                <h1>Não tenha <span>custo fixos</span> e deixe toda <span>burocracia</span> por nossa conta,
+                    oferecemos todo o suporte em <span>engenharia</span>
+                    e você ficará livre para focar nas vendas,
+                    expandido <span>sua empresa</span></h1>
+            </div>
+
+            <div class="whatss-INICIO">
+
+                <a href="https://wa.me/5585981764642">FALE CONOSCO</a>
+            </div>
+
 
         </div>
-
-        
-
-        <div class="objetivo">
-
-            <h1>Não tenha <span>custo fixos</span> e deixe toda <span>burocracia</span> por nossa conta,
-                oferecemos todo o suporte em <span>engenharia</span>
-                e você ficará livre para focar nas vendas,
-                expandido <span>sua empresa</span></h1>
-        </div>
-
-        </div>
-
-        
 
         <img src="img/projetos.png">
 
+    </div>
+
+    <div class="bloco-2">
+
+        
+        <h1  class="titulo-bloco-2">NOSSAS VANTAGENS</h1>
+
+        <div class="suporte">
+            <h1>SUPORTE COM ESPECIALISTA</h1>
+            <a>Todos suporte e consultoria, com engenheiro experiente</a>
         </div>
 
-    
+        <div class="velocidade">
+            <h1>VELOCIDADE NA APROVAÇÃO DE PROJETOS</h1>
+            <a>Nossa equipe presta serviço para todos os estados, temos uma vasta experiencia, e temos a equipe mais rapida a ingressar o seu projeto na conssecionaria</a>
+        </div>
+
+        <div class="preco">
+            <h1>MELHORES PREÇOS</h1>
+            <a>Temos a melhor equipe, com os melhores preços</a>
+        </div>
+    </div>
+
+
+
+
+
 
 
 
